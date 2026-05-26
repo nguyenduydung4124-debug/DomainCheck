ccfa32ef40c1c08734f439174da7127e97680b23
nguyenduydung4124-debug <nguyenduydung4124@gmail.com>
2026-05-26 10:09:43 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/DomainCheck/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
